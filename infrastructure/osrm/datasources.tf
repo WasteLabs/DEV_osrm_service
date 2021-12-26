7b769ef0561da4779f4ab308625714a6e093a34b
AtmosOne <adil.rashitov.98@gmail.com>
2021-12-26 17:59:13 +0600

    FIX: ec2 instance deployment & vpc output matching to parameters.

--- infrastructure/osrm/datasources.tf
+++ infrastructure/osrm/datasources.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket  = "waste-labs-terraform-infrastructure"
-    key     = "env:/dev/DEV_OSRM_SERVICE/vpc.tfstate"
-    region  = "us-east-1"
+    bucket  = "waste-labs-terraform-backends"
+    key     = "env:/${terraform.workspace}/DEV_OSRM_SERVICE/vpc.tfstate"
+    region  = var.tags.region
   }
 }
