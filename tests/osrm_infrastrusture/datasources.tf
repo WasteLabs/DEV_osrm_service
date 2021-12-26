99220eb93964c6ba9d40eb7fafd6b5d60bae2a16
AtmosOne <adil.rashitov.98@gmail.com>
2021-12-26 14:49:20 +0600

    REMOVE: unneeded code base after restructure.

--- tests/osrm_infrastrusture/datasources.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket  = "waste-labs-terraform-infrastructure"
-    key     = "services/test/osrm/osrm_vpc.tfstate"
-    region  = "eu-west-2"
-  }
-}
